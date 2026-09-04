922c9986314b303ef8fc8da979daad8ff131ca74
Mac <contact@maclong.dev>
2026-09-04 15:25:21 +0100

    Add code listings to data tutorials

---
+++ Sources/RobinExtensions/RobinPostgres/Documentation.docc/Tutorials/ConfigurePostgreSQL-01.swift
@@ -0,0 +1,12 @@
+import RobinPostgres
+
+func databaseConfiguration(
+  host: String,
+  username: String,
+  password: String?
+) -> PostgresConfiguration {
+  PostgresConfiguration(
+    host: host,
+    username: username,
+    password: password)
+}
